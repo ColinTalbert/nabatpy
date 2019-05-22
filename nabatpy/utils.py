44bee7e9068b2b58533f6f461d9b3b4908efb04c
Talbert <talbertc@usgs.gov>
2019-05-22 11:40:45 -0600

    code to convert the guano MD from a directory into a dataframe

--- nabatpy/utils.py
+++ nabatpy/utils.py
@@ -24,6 +24,11 @@ from pathlib import Path
 from datetime import datetime, timedelta
 import pandas as pd
 
+column_lookup = pd.read_csv(Path(__file__).parent.joinpath('resources', "NABatColumnRosetta.csv"))
+row_lookup = column_lookup[['bulk_upload_columns', 'df_columns', 'guano_tag2']].dropna()
+row_lookup_v2 = column_lookup[['df_columns', 'guano_tag2']].dropna()
+
+
 def normalize_grid_frame(grid_frame):
     """
     Given a range of acceptible abbreviations and spellings returns the exact frame name that we need.
@@ -130,6 +135,9 @@ def parse_nabat_fname(fname):
         grtsid += digit
         digit = name[0]
 
+    grtsid = grtsid.lstrip("0")
+
+
     if name.startswith('_'):
         name = name [1:]
     try:
@@ -143,7 +151,7 @@ def parse_nabat_fname(fname):
 
     dt = datetime.strptime('T'.join([datestr, timestr]), "%Y%m%dT%H%M%S")
     parts = {"GrtsId":grtsid, "SiteName":sitename, "datetime":dt}
-    parts['correct_fname'] =  parts_to_fname(parts)
+    parts['correct_fname'] = parts_to_fname(parts)
 
     return parts
 
@@ -153,7 +161,6 @@ def parts_to_fname(parts):
     return f"{parts['GrtsId']}_{parts['SiteName']}_{parts['datetime'].year}{dt.month:02d}{dt.day:02d}_{dt.hour:02d}{dt.minute:02d}{dt.second:02d}.wav"
 
 
-# get_site_coords
 def get_latlon(fname):
     p = Path(fname)
     try:
@@ -242,14 +249,15 @@ def bulkupload_to_df(bulk_upload_csv):
 
     lookup = pd.read_csv(Path(__file__).parent.joinpath('resources', "NABatColumnRosetta.csv"))
 
-    df = pd.read_csv(bulk_upload_csv)
+    df = pd.read_csv(bulk_upload_csv, parse_dates=['Survey Start Time', 'Survey End Time'])
 
     if df.iloc[0,0] == 'Integer -Required':
         df.drop(df.index[0])
 
-    df = df.rename(columns=dict(zip(lookup.bulk_upload_columns, lookup.df_columns)))
-    df.survey_start_time = pd.to_datetime(df.survey_start_time)
-    df.survey_end_time = pd.to_datetime(df.survey_end_time)
+    upload_columns = column_lookup[['bulk_upload_columns', 'df_columns']].dropna()
+
+    df = df.rename(columns=dict(zip(upload_columns.bulk_upload_columns,
+                                    upload_columns.df_columns)))
 
     return df
 
@@ -280,3 +288,162 @@ def df_to_bulkupload(df, bulk_upload_csv):
     df.to_csv(bulk_upload_csv, index=False)
 
     return None
+
+
+def generate_bulkupload(source_dname, redo_all=False, recursive=True, verbose=1):
+
+    d = Path(source_dname)
+    if redo_all:
+        if d.joinpath('_problems.csv').exists():
+            d.joinpath('_problems.csv').unlink()
+        if d.joinpath('_batchupload.csv').exists():
+            d.joinpath('_batchupload.csv').unlink()
+
+    df, problems = guano_to_df(source_dname, recursive=False, verbose=verbose)
+
+    if recursive:
+        for thing in d.glob('*'):
+            if thing.is_dir():
+                sub_df, sub_problems = generate_bulkupload(thing, recursive=False, verbose=verbose)
+                if df is None and sub_df is not None:
+                    df = sub_df
+                elif sub_df is not None:
+                    df.append(sub_df, sort=False)
+
+                if problems is None and sub_problems is not None:
+                    problems = sub_problems
+                elif sub_problems is not None:
+                    problems.append(sub_df, sort=False)
+
+    if df is not None:
+        df_to_bulkupload(df, d.joinpath('_batchupload.csv'))
+
+    if problems is not None and problems.shape[0] != 0:
+        df_to_bulkupload(problems, d.joinpath('_problems.csv'))
+
+    return df, problems
+
+
+def guano_to_df(source_dname, recursive=True, verbose=1, use_previous=True):
+    """Create an NABat bulk upload csv from the MD contained in a folder of wav files
+    """
+    d = Path(source_dname)
+
+    if use_previous and d.joinpath('_problems.csv').exists():
+        problems_df = bulkupload_to_df(d.joinpath('_problems.csv'))
+    else:
+        problems_df = None
+
+    if use_previous and d.joinpath('_batchupload.csv').exists():
+        df = bulkupload_to_df(d.joinpath('_batchupload.csv'))
+    else:
+        df = None
+
+    if not use_previous or df is None:
+        wavs = list(d.glob('*.wav'))
+        if len(wavs) > 0:
+            if verbose >= 1:
+                print(f'Starting on {source_dname}')
+            if dask is not None:
+                values = [delayed(get_row_from_guano)(wav) for wav in wavs]
+                if verbose >= 1:
+                    with ProgressBar():
+                        results = compute(*values, scheduler='processes')
+                else:
+                    results = compute(*values, scheduler='processes')
+            else:
+                results = [get_row_from_guano(wav) for wav in wavs]
+
+            df = pd.DataFrame.from_records(results)
+            problems_df = df[df.detector=='Problem extracting row from Guano']
+            df = df[df.detector!='Problem extracting row from Guano']
+
+        if recursive:
+            for thing in d.glob('*'):
+                if thing.is_dir():
+                    # print(thing)
+                    sub_df, sub_problems = guano_to_df(thing)
+                    if df is None:
+                        df = sub_df
+                    else:
+                        # print('else')
+                        df = df.append(sub_df, sort=False)
+
+                    if problems_df is None and sub_problems is not None:
+                        problems_df = sub_problems
+                    elif sub_problems is not None:
+                        problems_df = problems_df.append(sub_df, sort=False)
+
+    return df, problems_df
+
+
+def get_empty_row(version=1):
+    row = OrderedDict()
+
+    if version == 2:
+        for i, keyvalue in row_lookup_v2.iterrows():
+            row[keyvalue.df_columns] = ''
+    else:
+        for i, keyvalue in row_lookup.iterrows():
+            row[keyvalue.df_columns] = ''
+
+    return row
+
+
+def get_row_from_guano(fname):
+
+    row = get_empty_row()
+
+    try:
+        g = GuanoFile(fname)
+        for i, keyvalue in row_lookup.iterrows():
+            value = g.get(keyvalue.guano_tag2, '')
+            if value.lower() == 'nan':
+                value = ''
+            row[keyvalue.df_columns] = value
+
+        if g.get('NABat|Site coordinates'):
+            #     print('.')
+            lat, long = g.get('NABat|Site coordinates').split()
+            row['latitude'] = lat
+            row['longitude'] = long
+
+        # parse the software type from vendor namespaces
+        if 'SB' in g.get_namespaces():
+            software = 'Sonobat '
+            if g.get('SB|Version').startswith('4.2'):
+                software += '4.2'
+            elif g.get('SB|Version').startswith('4.'):
+                software += '4.x'
+            elif g.get('SB|Version').startswith('3.'):
+                software += '3.x'
+            row['software_type'] = software
+        else:
+            # TODO: add logic for Kaleidoscope
+            pass
+
+        # Make sure we're using a auto/manual ID if available
+        if row['auto_id'] == '':
+            row['auto_id'] = g.get('GUANO|Species Auto ID', '')
+
+        if row['manual_id'] == '':
+            row['manual_id'] = g.get('GUANO|Species Manual ID', '')
+
+        # convert nan to empty string
+        for which in ['auto', 'manual']:
+            if row[f'{which}_id'].lower() == 'nan':
+                row[f'{which}_id'] = ''
+    except:
+        # Something went dreadfully wrong. We'll populate with what we have
+        parts = parse_nabat_fname(fname)
+        row['grts_cell_id'] = parts['GrtsId']
+        row['location_name'] = parts['SiteName']
+        row['detector'] = "Problem extracting row from Guano"
+
+    row['audio_recording_name'] = Path(fname).name
+
+    return row
+
+
+def add_monitoringnight(series):
+    pass
